5c4c1402020d4be416a62033d98eb150cc193dc2
Josafat Morales <joex45125@gmail.com>
2021-05-04 07:04:07 -0500

    se crearon los modales

---
+++ render-functions/App.vue
@@ -0,0 +1,58 @@
+<template>
+ <div id="app">
+   <button v-on:click.prevent="toggleHandler">
+     Toogle modal
+   </button>
+<!--   <modal-create-element v-bind:show="show">-->
+<!--     <p>Hello world</p>-->
+<!--   </modal-create-element>-->
+   <modal-jsx v-bind:show="show">
+     <p>Hello world JSX</p>
+   </modal-jsx>
+ </div>
+</template>
+
+<script>
+// import ModalCreateElement from './ModalCreateElement.vue';
+import ModalJsx from './ModalJsx.vue';
+
+export default {
+  name: 'App',
+  data() {
+    return {
+      show: false,
+    };
+  },
+  methods: {
+    toggleHandler() {
+      this.show = !this.show;
+    },
+  },
+  components: {
+    // ModalCreateElement,
+    ModalJsx,
+  },
+};
+</script>
+
+<style scoped>
+body {
+  background: #cecece;
+}
+
+.modal {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  margin-right: -50%;
+  transform: translate(-50%, -50%);
+  width: 250px;
+  background: #fff;
+  padding: 15px;
+}
+
+.modal > .message {
+  margin: 0;
+  margin-bottom: 10px;
+}
+</style>
---
+++ render-functions/ModalJsx.vue
@@ -0,0 +1,35 @@
+<script>
+export default {
+  name: 'ModalJsx',
+  render() {
+    return (this.show) ? <div class="modal">
+      <div class="content" >{ this.$slots.default }</div>
+      {/* COMENTARIOS EN JSX */}
+      <button onClick={this.clickCancelHandler}>Cancel</button>
+      <button onClick={this.clickOkHandler}>Ok</button>
+    </div> : null;
+  },
+  props: {
+    show: {
+      type: Boolean,
+      default: false,
+    },
+    // message: {
+    //   type: String,
+    //   required: true,
+    // },
+  },
+  methods: {
+    clickCancelHandler() {
+      console.log('Click on cancel');
+    },
+    clickOkHandler() {
+      console.log('Click on Ok');
+    },
+  },
+};
+</script>
+
+<style scoped>
+
+</style>
